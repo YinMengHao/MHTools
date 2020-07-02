fb1f3e4e1c8900013b5a81a4c533742e231ea403
YinMengHao <729263080@qq.com>
2020-07-02 14:16:42 +0800

    添加工具类头文件

---
+++ MHTools/Classes/Tools/Unit/MHTools.h
@@ -0,0 +1,33 @@
+//
+//  MHTools.h
+//  MHLogin
+//
+//  Created by a on 2020/7/2.
+//  Copyright © 2020 a. All rights reserved.
+//
+
+#ifndef MHTools_h
+#define MHTools_h
+
+#import "MHDateChange.h"
+#import "MHCreateViewTool.h"
+#import "HQGeneralUtil.h"
+#import "MHBaseScreen.h"
+#import "MHBaseRequest.h"
+
+
+#import "NSString+NSStringUtil.h"
+#import "UIImage+Extention.h"
+#import "UIColor+Hex.h"
+#import "UIButton+MHExtension.h"
+#import "UIView+MHFrame.h"
+#import "UIView+Toast.h"
+#import "UIView+MHExtension.h"
+#import "UICollectionViewCell+Extension.h"
+#import "UITableViewCell+Extension.h"
+#import "UIViewController+Swizzling.h"
+
+
+
+
+#endif /* MHTools_h */
