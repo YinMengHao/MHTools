c753a6696c39c795d1d38520199c3bbc4a308ff6
YinMengHao <729263080@qq.com>
2020-07-02 11:39:52 +0800

    first commit

---
+++ Example/Pods/Target Support Files/MHTools/MHTools-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MHTools : NSObject
+@end
+@implementation PodsDummy_MHTools
+@end
---
+++ Example/Pods/Target Support Files/Pods-MHTools_Tests/Pods-MHTools_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MHTools_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MHTools_Tests
+@end
